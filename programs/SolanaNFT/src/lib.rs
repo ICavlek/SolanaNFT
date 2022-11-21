d1910643fca30584aa57d72996bd5396254c0e64
IvanC <ivan.cavlek1403@gmail.com>
2022-11-21 11:01:52 +0100

    Rust analyzer works now

--- programs/SolanaNFT/src/lib.rs
+++ programs/SolanaNFT/src/lib.rs
@@ -1,4 +1,8 @@
-use anchor_lang::prelude::*;
+use {
+    anchor_lang::{prelude::*, solana_program::program::invoke, system_program},
+    anchor_spl::{associated_token, token},
+    mpl_token_metadata::{instruction as token_instruction, ID as TOKEN_METADATA_ID},
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +10,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_nft {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint(ctx: Context<MintNFT>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintNFT<'info> {
+    /// CHECK: We are about to create this with Metaplex
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+}
