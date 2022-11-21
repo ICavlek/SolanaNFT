de78c864258c30348306dfb6d94ea2b4795598c6
IvanC <ivan.cavlek1403@gmail.com>
2022-11-21 11:45:12 +0100

    Moved some stuff to function, not parts with invoke due to some rust complaint

--- programs/SolanaNFT/src/lib.rs
+++ programs/SolanaNFT/src/lib.rs
@@ -16,65 +16,10 @@ pub mod solana_nft {
         metadata_symbol: String,
         metadata_uri: String,
     ) -> Result<()> {
-        msg!("Creating mint account...");
-        msg!("Mint: {}", &ctx.accounts.mint.key());
-        system_program::create_account(
-            CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                system_program::CreateAccount {
-                    from: ctx.accounts.mint_authority.to_account_info(),
-                    to: ctx.accounts.mint.to_account_info(),
-                },
-            ),
-            10000000,
-            82,
-            &ctx.accounts.token_program.key(),
-        )?;
-
-        msg!("Initializing mint account...");
-        msg!("Mint: {}", &ctx.accounts.mint.key());
-        token::initialize_mint(
-            CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                token::InitializeMint {
-                    mint: ctx.accounts.mint.to_account_info(),
-                    rent: ctx.accounts.rent.to_account_info(),
-                },
-            ),
-            0,
-            &ctx.accounts.mint_authority.key(),
-            Some(&ctx.accounts.mint_authority.key()),
-        )?;
-
-        msg!("Creating token account...");
-        msg!("Token Address: {}", &ctx.accounts.token_account.key());
-        associated_token::create(CpiContext::new(
-            ctx.accounts.associated_token_program.to_account_info(),
-            associated_token::Create {
-                payer: ctx.accounts.mint_authority.to_account_info(),
-                associated_token: ctx.accounts.token_account.to_account_info(),
-                authority: ctx.accounts.mint_authority.to_account_info(),
-                mint: ctx.accounts.mint.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-                token_program: ctx.accounts.token_program.to_account_info(),
-                rent: ctx.accounts.rent.to_account_info(),
-            },
-        ))?;
-
-        msg!("Minting token to token account...");
-        msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key());
-        msg!("Token Address: {}", &ctx.accounts.token_account.key());
-        token::mint_to(
-            CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                token::MintTo {
-                    mint: ctx.accounts.mint.to_account_info(),
-                    to: ctx.accounts.token_account.to_account_info(),
-                    authority: ctx.accounts.mint_authority.to_account_info(),
-                },
-            ),
-            1,
-        )?;
+        create_mint_account(&ctx)?;
+        initialize_mint_account(&ctx)?;
+        create_token_account(&ctx)?;
+        mint_token_to_account(&ctx)?;
 
         msg!("Creating metadata account...");
         msg!(
@@ -161,4 +106,72 @@ pub struct MintNFT<'info> {
     pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
     /// CHECK: Metaplex will check this
     pub token_metadata_program: UncheckedAccount<'info>,
+}
+
+fn create_mint_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Creating mint account...");
+    msg!("Mint: {}", &ctx.accounts.mint.key());
+    system_program::create_account(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            system_program::CreateAccount {
+                from: ctx.accounts.mint_authority.to_account_info(),
+                to: ctx.accounts.mint.to_account_info(),
+            },
+        ),
+        10000000,
+        82,
+        &ctx.accounts.token_program.key(),
+    )
+}
+
+fn initialize_mint_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Initializing mint account...");
+    msg!("Mint: {}", &ctx.accounts.mint.key());
+    token::initialize_mint(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::InitializeMint {
+                mint: ctx.accounts.mint.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+        ),
+        0,
+        &ctx.accounts.mint_authority.key(),
+        Some(&ctx.accounts.mint_authority.key()),
+    )
+}
+
+fn create_token_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Creating token account...");
+    msg!("Token Address: {}", &ctx.accounts.token_account.key());
+    associated_token::create(CpiContext::new(
+        ctx.accounts.associated_token_program.to_account_info(),
+        associated_token::Create {
+            payer: ctx.accounts.mint_authority.to_account_info(),
+            associated_token: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.mint_authority.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            token_program: ctx.accounts.token_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+        },
+    ))
+}
+
+fn mint_token_to_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Minting token to token account...");
+    msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key());
+    msg!("Token Address: {}", &ctx.accounts.token_account.key());
+    token::mint_to(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
+            },
+        ),
+        1,
+    )
 }
