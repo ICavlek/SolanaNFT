5993b3e0f9bf9337db456b6651a6b0209c83f15a
IvanC <ivan.cavlek1403@gmail.com>
2022-11-22 10:15:18 +0100

    Moved everything to functions

--- programs/SolanaNFT/src/lib.rs
+++ programs/SolanaNFT/src/lib.rs
@@ -1,3 +1,5 @@
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
 use {
     anchor_lang::{prelude::*, solana_program::program::invoke, system_program},
     anchor_spl::{associated_token, token},
@@ -20,67 +22,9 @@ pub mod solana_nft {
         initialize_mint_account(&ctx)?;
         create_token_account(&ctx)?;
         mint_token_to_account(&ctx)?;
-
-        msg!("Creating metadata account...");
-        msg!(
-            "Metadata account address: {}",
-            &ctx.accounts.metadata.to_account_info().key()
-        );
-        invoke(
-            &token_instruction::create_metadata_accounts_v2(
-                TOKEN_METADATA_ID,
-                ctx.accounts.metadata.key(),
-                ctx.accounts.mint.key(),
-                ctx.accounts.mint_authority.key(),
-                ctx.accounts.mint_authority.key(),
-                ctx.accounts.mint_authority.key(),
-                metadata_title,
-                metadata_symbol,
-                metadata_uri,
-                None,
-                1,
-                true,
-                false,
-                None,
-                None,
-            ),
-            &[
-                ctx.accounts.metadata.to_account_info(),
-                ctx.accounts.mint.to_account_info(),
-                ctx.accounts.token_account.to_account_info(),
-                ctx.accounts.mint_authority.to_account_info(),
-                ctx.accounts.rent.to_account_info(),
-            ],
-        )?;
-
-        msg!("Creating master edition metadata account...");
-        msg!(
-            "Master edition metadata account address: {}",
-            &ctx.accounts.master_edition.to_account_info().key()
-        );
-        invoke(
-            &token_instruction::create_master_edition_v3(
-                TOKEN_METADATA_ID,
-                ctx.accounts.master_edition.key(),
-                ctx.accounts.mint.key(),
-                ctx.accounts.mint_authority.key(),
-                ctx.accounts.mint_authority.key(),
-                ctx.accounts.metadata.key(),
-                ctx.accounts.mint_authority.key(),
-                Some(0),
-            ),
-            &[
-                ctx.accounts.master_edition.to_account_info(),
-                ctx.accounts.metadata.to_account_info(),
-                ctx.accounts.mint.to_account_info(),
-                ctx.accounts.token_account.to_account_info(),
-                ctx.accounts.mint_authority.to_account_info(),
-                ctx.accounts.rent.to_account_info(),
-            ],
-        )?;
-
+        create_metadata_account(&ctx, metadata_title, metadata_symbol, metadata_uri)?;
+        create_master_edition_metadata_account(&ctx)?;
         msg!("Token mint process completed successfully.");
-
         Ok(())
     }
 }
@@ -173,5 +117,72 @@ fn mint_token_to_account(ctx: &Context<MintNFT>) -> Result<()> {
             },
         ),
         1,
+    )
+}
+
+fn create_metadata_account(
+    ctx: &Context<MintNFT>,
+    metadata_title: String,
+    metadata_symbol: String,
+    metadata_uri: String,
+) -> ProgramResult {
+    msg!("Creating metadata account...");
+    msg!(
+        "Metadata account address: {}",
+        &ctx.accounts.metadata.to_account_info().key()
+    );
+    invoke(
+        &token_instruction::create_metadata_accounts_v2(
+            TOKEN_METADATA_ID,
+            ctx.accounts.metadata.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.mint_authority.key(),
+            metadata_title,
+            metadata_symbol,
+            metadata_uri,
+            None,
+            1,
+            true,
+            false,
+            None,
+            None,
+        ),
+        &[
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_account.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+    )
+}
+
+fn create_master_edition_metadata_account(ctx: &Context<MintNFT>) -> ProgramResult {
+    msg!("Creating master edition metadata account...");
+    msg!(
+        "Master edition metadata account address: {}",
+        &ctx.accounts.master_edition.to_account_info().key()
+    );
+    invoke(
+        &token_instruction::create_master_edition_v3(
+            TOKEN_METADATA_ID,
+            ctx.accounts.master_edition.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.metadata.key(),
+            ctx.accounts.mint_authority.key(),
+            Some(0),
+        ),
+        &[
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_account.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
     )
 }
