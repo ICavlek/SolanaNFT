dd864a8ab30c2f47309ad3354dbfbb7c4b21734f
IvanC <ivan.cavlek1403@gmail.com>
2022-11-22 11:47:56 +0100

    Added typescript and NFT json example

--- tests/SolanaNFT.ts
+++ tests/SolanaNFT.ts
@@ -3,14 +3,67 @@ import { Program } from "@project-serum/anchor";
 import { SolanaNft } from "../target/types/solana_nft";
 
 describe("SolanaNFT", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaNft as Program<SolanaNft>;
+  const testNFTTitle = "Dinamo NFT!";
+  const testNFTSymbol = "DZG";
+  const testNFTUri = "https://raw.githubusercontent.com/Coding-and-Crypto/Rust-Solana-Tutorial/master/nfts/mint-nft/assets/example.json";
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const provider = anchor.AnchorProvider.env();
+  const wallet = provider.wallet as anchor.Wallet;
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.SolanaNft as anchor.Program<SolanaNft>;
+
+  const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  ); // Don't know what that is, googled and extracted from metaplex
+
+  it("Mint!", async () => {
+    // Derive the mint address and the associated token account address
+
+    const mintKeypair: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const tokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mintKeypair.publicKey,
+      owner: wallet.publicKey
+    });
+    console.log(`New token: ${mintKeypair.publicKey}`);
+
+    // Derive the metadata and master edition addresses
+
+    const metadataAddress = (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mintKeypair.publicKey.toBuffer(),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0];
+    console.log("Metadata initialized");
+    const masterEditionAddress = (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mintKeypair.publicKey.toBuffer(),
+        Buffer.from("edition"),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0];
+    console.log("Master edition metadata initialized");
+
+    // Transact with the "mint" function in our on-chain program
+
+    await program.methods.mint(
+      testNFTTitle, testNFTSymbol, testNFTUri
+    )
+      .accounts({
+        masterEdition: masterEditionAddress,
+        metadata: metadataAddress,
+        mint: mintKeypair.publicKey,
+        tokenAccount: tokenAddress,
+        mintAuthority: wallet.publicKey,
+        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+      })
+      .signers([mintKeypair])
+      .rpc();
   });
 });
