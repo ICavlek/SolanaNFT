1e730079808707a3e02873a4d2321663889812f3
IvanC <ivan.cavlek1403@gmail.com>
2022-11-25 10:28:55 +0100

    Updated public keys as databases

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -11,7 +11,13 @@ export async function createKeypairFromFile(
 }
 
 export function getNFTPublicKey(NFTToken: string): anchor.web3.PublicKey {
+    const dataBase: { [name: string]: string } = {
+        "Dinamo Zagreb NFT!": "5cLA8rWgouBLVAFC7f5XUjLxK2SJ7CwMMvfMHNy7MRP4",
+        "NFT Dinamo": "C2otQeNVfi2wkaeLPpm8rxSajgbgJSgumDXSC2eVbcaR",
+        "NFT Hajduk": "HCvebUTFJbk4qpsj1YN9WkFB3Nx2u3uh96x8dnzDF1XJ",
+        "NFT Osijek": "D4AYQKhkstrbK7PoAqpwqs9WCDMU8SG2g1pZG4ppoJoX"
+    };
     return new anchor.web3.PublicKey(
-        "5cLA8rWgouBLVAFC7f5XUjLxK2SJ7CwMMvfMHNy7MRP4"
+        dataBase[NFTToken]
     );
 }
